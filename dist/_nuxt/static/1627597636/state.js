b290eef4d1697920b64ea3d0b46c097e5e46c262
unknown <ridoananda123@gmail.com>
2021-08-01 03:43:03 +0700

    upload file for nuxt static

--- dist/_nuxt/static/1627597636/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdist\u002F_nuxt\u002Fstatic\u002F1627597636",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002F",config:{_app:{basePath:"\u002Fdist\u002F",assetsPath:"\u002Fdist\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"becd7d28"}}}}(null));
